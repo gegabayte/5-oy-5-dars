e1f2a68234787fd9009f793539ea2d9a1341af3b
gegabayte <xasanboymuhammaddinov41@gmail.com>
2024-01-21 16:15:30 +0500

    chenhed

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//1-misol
+// 1 dan  100 gacha bolgan sonlarni ekranga chiqaruvchi algoritm yozing.
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+
+//2-misol
+// 1 dan  n  gcha  bolgan  sonlarni ekranga  chiqaruvchi  dastur yozing.
+// let n = +prompt(`ixtiyoriy sonn kiriting`)
+// for (let i = 1; i <=n; i++) {
+//     console.log(i)
+// }
